639aac069beab99a93a99888ea7dd4498074568c
Joseph Gao <gaoj@seas.upenn.edu>
2016-02-27 17:56:29 -0500

    search works

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  
+
+  # search
+  get '/search' => 'events#search'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
