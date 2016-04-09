46ce0f2d2ec3cedc4cb100c6f8a9e24c120c6697
Adam Domingoes <adamdom@wharton.upenn.edu>
2016-04-09 18:00:56 -0400

    FAQ updates and scroll fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
 
     def faqs()
         f = []
-        f.push(["What is Pennvolvement?","Pennvolvement is a web platform for companies and organizations to post volunteer opportunities for University of Pennsylvania students."])
-        f.push(["I am an organization how do I use Pennvolvement?","Go to the home page and fill out your information to sign up. Then go to 'Create a Post' to post your opportunity. To edit or delete your listing, vist my profile, where you can also update your information."])
-        f.push(["I am a student. How do I use Pennvolvement?","There is no need to make an account. Simply browse using the opportunities tab. You may also search using the search bar or filter opportunities based on category."])
+        f.push([1, "What is Pennvolvement?","Pennvolvement is a web platform for companies and organizations to post volunteer opportunities for University of Pennsylvania students."])
+        f.push([2, "I am an organization how do I use Pennvolvement?","Go to the home page and fill out your information to sign up. Then go to 'Create a Post' to post your opportunity. To edit or delete your listing, vist my profile, where you can also update your information."])
+        f.push([3, "I am a student. How do I use Pennvolvement?","There is no need to make an account. Simply browse using the opportunities tab. You may also search using the search bar or filter opportunities based on category."])
     end
 end
