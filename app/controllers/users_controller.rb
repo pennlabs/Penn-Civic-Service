cb1cc851c71af7052a0be5b227799d7a192cbeec
Joseph Gao <gaoj@seas.upenn.edu>
2016-04-19 21:27:13 -0400

    added password confirmation profile update page, fixes #17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
-        format.html { render :edit }
+        format.html { render :template => "users/show"  }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
