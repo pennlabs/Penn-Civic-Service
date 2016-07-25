8df02f8216038ad8aae8716a06f653028f07dc71
Adam Domingoes <adamdom@wharton.upenn.edu>
2016-07-25 10:30:18 -0400

    Typo Fix

--- app/controllers/user_mailer.rb
+++ app/controllers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
 	def registration_confirmation(user)
 		@user = user
 		msg = "Hello " + "#{user.firstname}, \n\n" +
-                "Click the following link to finish setting up your accounti:\n \n" +
+                "Click the following link to finish setting up your account:\n \n" +
 		"http://pennvolvement.com/confirm?c=#{user.confirm_token}"
 		`echo "#{msg}" | mailx -s "Pennvovlement Email Confirmation" #{user.email} -aFrom:noreply@pennvolvement.com`
 	end
