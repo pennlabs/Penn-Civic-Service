dd21d3fec605f093890aa51fdec649b46a0d6b76
adamdom <adamdom@wharton.upenn.edu>
2016-07-26 10:06:06 -0400

    Style and bug fixes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
   validates :date, presence: true
-  validates :deadline, presence: true
+  validates :deadline, presence: false
   validates :time, presence: true
   validates :location, presence: true
   validates :url, presence: true
