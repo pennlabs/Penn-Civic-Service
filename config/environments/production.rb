833b4043890651ff28e2d5683abdaf1283f7fcb7
Adel Qalieh <aqalieh95@gmail.com>
2016-10-11 18:49:30 -0400

    Move config into correct file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.smtp_settings = {
+	  :address              => "smtp.sendgrid.net",
+	  :port                 => 587,
+	  :user_name            => ENV['SENDGRID_USERNAME'],
+	  :password             => ENV['SENDGRID_PASSWORD'],
+	  :authentication       => "plain",
+	  :enable_starttls_auto => true
+  }
 end
