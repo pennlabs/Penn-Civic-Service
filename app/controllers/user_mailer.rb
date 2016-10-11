8d9a24e9f54ac97bc2ff1cbfa4266d0f007725f5
Adel Qalieh <aqalieh95@gmail.com>
2016-10-11 18:13:13 -0400

    Fix user mailer controller

--- app/controllers/user_mailer.rb
+++ app/controllers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@pennvolvement.com"
-  layout 'registration_confirmation'
 
 	def registration_confirmation(user)
+    @user = user
     @url = "https://pennvolvement.com/confirm?c=#{user.confirm_token}"
     mail(to: user.email, subject: 'Pennvolvement Email Confirmation')
 	end
