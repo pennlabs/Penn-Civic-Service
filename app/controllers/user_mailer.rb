e5d2e0533d937cb40f87de86e723a605ba1f14d2
Adel Qalieh <aqalieh95@gmail.com>
2016-12-07 17:02:11 -0500

    Fix indentation and punctuation in user_mailer

--- app/controllers/user_mailer.rb
+++ app/controllers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "noreply@pennvolvement.com"
+  default from: 'noreply@pennvolvement.com'
 
-	def registration_confirmation(user)
+  def registration_confirmation(user)
     @user = user
     @url = "https://pennvolvement.com/confirm?c=#{user.confirm_token}"
     mail(to: user.email, subject: 'Pennvolvement Email Confirmation')
-	end
+  end
 end
